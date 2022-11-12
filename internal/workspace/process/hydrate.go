0775de67162abeba975d0000f450fab9394547bc
Xabier Larrakoetxea <me@slok.dev>
2022-11-12 15:23:51 +0100

    Add latest drift detection hydrate processor
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

---
+++ internal/workspace/process/hydrate.go
@@ -0,0 +1,39 @@
+package process
+
+import (
+	"context"
+	"errors"
+
+	"github.com/slok/tfe-drift/internal/internalerrors"
+	"github.com/slok/tfe-drift/internal/log"
+	"github.com/slok/tfe-drift/internal/model"
+)
+
+type WorkspaceLatestCheckPlanGetter interface {
+	GetLatestCheckPlan(ctx context.Context, w model.Workspace) (*model.Plan, error)
+}
+
+//go:generate mockery --case underscore --output processmock --outpkg processmock --name WorkspaceLatestCheckPlanGetter
+
+func NewHydrateLatestDetectionPlanProcessor(logger log.Logger, g WorkspaceLatestCheckPlanGetter) Processor {
+	logger = logger.WithValues(log.Kv{"workspace-processor": "HydrateLatestDetectionPlan"})
+
+	return ProcessorFunc(func(ctx context.Context, wks []model.Workspace) ([]model.Workspace, error) {
+		logger.Infof("Getting Workspaces' latest drift detection")
+
+		// TODO(slok): Add concurrency.
+		newWks := []model.Workspace{}
+		for _, wk := range wks {
+			plan, err := g.GetLatestCheckPlan(ctx, wk)
+			if err != nil && !errors.Is(err, internalerrors.ErrNotExist) {
+				// TODO(slok): Add strict as an option so we can fail or not based on this option.
+				// Don't stop all the  process for other workspaces because of one workspace error.
+				logger.WithValues(log.Kv{"workspace": wk.ID}).Errorf("could not get latest drift detection plan for workspaces %q: %w", wk.Name, err)
+			}
+			wk.LastDriftPlan = plan
+			newWks = append(newWks, wk)
+		}
+
+		return newWks, nil
+	})
+}
---
+++ internal/workspace/process/hydrate_test.go
@@ -0,0 +1,75 @@
+package process_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/slok/tfe-drift/internal/log"
+	"github.com/slok/tfe-drift/internal/model"
+	"github.com/slok/tfe-drift/internal/workspace/process"
+	"github.com/slok/tfe-drift/internal/workspace/process/processmock"
+)
+
+func TestHydrateLatestDetectionPlanProcessor(t *testing.T) {
+	tests := map[string]struct {
+		mock          func(mg *processmock.WorkspaceLatestCheckPlanGetter)
+		workspaces    []model.Workspace
+		expWorkspaces []model.Workspace
+		expErr        bool
+	}{
+		"Not having workspaces shouldn't fail.": {
+			mock:          func(mg *processmock.WorkspaceLatestCheckPlanGetter) {},
+			workspaces:    []model.Workspace{},
+			expWorkspaces: []model.Workspace{},
+		},
+
+		"Having workspaces should hydrate with the latest drift detection plans.": {
+			mock: func(mg *processmock.WorkspaceLatestCheckPlanGetter) {
+				mg.On("GetLatestCheckPlan", mock.Anything, model.Workspace{ID: "wk1"}).Once().Return(&model.Plan{ID: "p1"}, nil)
+				mg.On("GetLatestCheckPlan", mock.Anything, model.Workspace{ID: "wk2"}).Once().Return(&model.Plan{ID: "p2"}, nil)
+				mg.On("GetLatestCheckPlan", mock.Anything, model.Workspace{ID: "wk3"}).Once().Return(&model.Plan{ID: "p3"}, nil)
+			},
+			workspaces: []model.Workspace{{ID: "wk1"}, {ID: "wk2"}, {ID: "wk3"}},
+			expWorkspaces: []model.Workspace{
+				{ID: "wk1", LastDriftPlan: &model.Plan{ID: "p1"}},
+				{ID: "wk2", LastDriftPlan: &model.Plan{ID: "p2"}},
+				{ID: "wk3", LastDriftPlan: &model.Plan{ID: "p3"}},
+			},
+		},
+
+		"Having an error while getting latest drift detection plans should stop the process.": {
+			mock: func(mg *processmock.WorkspaceLatestCheckPlanGetter) {
+				mg.On("GetLatestCheckPlan", mock.Anything, model.Workspace{ID: "wk1"}).Once().Return(&model.Plan{ID: "p1"}, nil)
+				mg.On("GetLatestCheckPlan", mock.Anything, model.Workspace{ID: "wk2"}).Once().Return(nil, fmt.Errorf("something"))
+				mg.On("GetLatestCheckPlan", mock.Anything, model.Workspace{ID: "wk3"}).Once().Return(&model.Plan{ID: "p3"}, nil)
+			},
+			workspaces: []model.Workspace{{ID: "wk1"}, {ID: "wk2"}, {ID: "wk3"}},
+			expWorkspaces: []model.Workspace{
+				{ID: "wk1", LastDriftPlan: &model.Plan{ID: "p1"}},
+				{ID: "wk2"},
+				{ID: "wk3", LastDriftPlan: &model.Plan{ID: "p3"}},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			mc := processmock.NewWorkspaceLatestCheckPlanGetter(t)
+			test.mock(mc)
+
+			p := process.NewHydrateLatestDetectionPlanProcessor(log.Noop, mc)
+			gotWks, err := p.Process(context.TODO(), test.workspaces)
+
+			if test.expErr {
+				assert.Error(err)
+			} else if assert.NoError(err) {
+				assert.Equal(test.expWorkspaces, gotWks)
+			}
+		})
+	}
+}
