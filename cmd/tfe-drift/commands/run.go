e6d43b7bc8e3da510459aec050879048fd5dda00
Xabier Larrakoetxea <me@slok.dev>
2022-11-14 08:10:19 +0100

    Add commands
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

---
+++ cmd/tfe-drift/commands/run.go
@@ -0,0 +1,132 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/go-tfe"
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	tfestorage "github.com/slok/tfe-drift/internal/storage/tfe"
+	"github.com/slok/tfe-drift/internal/workspace/process"
+	wksprocess "github.com/slok/tfe-drift/internal/workspace/process"
+)
+
+var (
+	waitPolling = 15 * time.Second
+
+	outFormatJSON = "json"
+)
+
+type RunCommand struct {
+	cmd        *kingpin.CmdClause
+	rootConfig *RootCommand
+
+	planMessage          string
+	includeNameRegexes   []string
+	excludeNameRegexes   []string
+	notBefore            time.Duration
+	maxPlans             int
+	waitTimeout          time.Duration
+	disableDriftExitCode bool
+	outFormat            string
+}
+
+// NewRunCommand returns the Run command.
+func NewRunCommand(rootConfig *RootCommand, app *kingpin.Application) *RunCommand {
+	cmd := app.Command("run", "Runs drift detections.")
+	c := &RunCommand{
+		cmd:        cmd,
+		rootConfig: rootConfig,
+	}
+
+	cmd.Flag("plan-message", "Message to set on the executed drift detection plans.").Short('m').Default("Drift detection").StringVar(&c.planMessage)
+	cmd.Flag("include-name", "Regex that if matches workspace name it will be included in the drift detection (can be repeated).").Short('i').StringsVar(&c.includeNameRegexes)
+	cmd.Flag("exclude-name", "Regex that if matches workspace name it will be excluded from the drift detection (can be repeated)").Short('e').StringsVar(&c.excludeNameRegexes)
+	cmd.Flag("limit-max-plans", "The maximum drift detection plans that will be executed").Short('l').IntVar(&c.maxPlans)
+	cmd.Flag("not-before", "Will filter the workspaces that executed a drift detection plan before before this duration").Short('n').Default("1h").DurationVar(&c.notBefore)
+	cmd.Flag("wait-timeout", "Max time duration to wait for drift detection plans to finish").Default("2h").DurationVar(&c.waitTimeout)
+	cmd.Flag("disable-drift-exitcode", "Will disable the exit code (!0) when there are changes on a drift detection plan").BoolVar(&c.disableDriftExitCode)
+	cmd.Flag("out-format", "Selects the format of the result output").EnumVar(&c.outFormat, outFormatJSON)
+
+	return c
+}
+
+func (c RunCommand) Name() string { return c.cmd.FullCommand() }
+func (c RunCommand) Run(ctx context.Context) error {
+	logger := c.rootConfig.Logger
+
+	if len(c.excludeNameRegexes) > 0 && len(c.includeNameRegexes) > 0 {
+		return fmt.Errorf("include and exclude name options can't be used at the same time")
+	}
+
+	config := &tfe.Config{
+		Token:   c.rootConfig.TFEToken,
+		Address: c.rootConfig.TFEAddress,
+	}
+
+	client, err := tfe.NewClient(config)
+	if err != nil {
+		return err
+	}
+
+	// Prepare processor chain.
+	repoTFEClient := tfestorage.NewClient(client)
+	repo, err := tfestorage.NewRepository(repoTFEClient, c.rootConfig.TFEOrg, c.rootConfig.TFEAddress, c.rootConfig.AppID)
+	if err != nil {
+		return fmt.Errorf("could not create tfe storage repository: %w", err)
+	}
+
+	var includeProcessor process.Processor = process.NoopProcessor
+	if len(c.includeNameRegexes) > 0 {
+		p, err := wksprocess.NewIncludeNameProcessor(logger, c.includeNameRegexes)
+		if err != nil {
+			return fmt.Errorf("invalid include processor: %w", err)
+		}
+		includeProcessor = p
+	}
+
+	var excludeProcessor process.Processor = process.NoopProcessor
+	if len(c.excludeNameRegexes) > 0 {
+		p, err := wksprocess.NewExcludeNameProcessor(logger, c.excludeNameRegexes)
+		if err != nil {
+			return fmt.Errorf("invalid exclude processor: %w", err)
+		}
+		excludeProcessor = p
+	}
+
+	var resultOutProcessor process.Processor = process.NoopProcessor
+	switch c.outFormat {
+	case outFormatJSON:
+		resultOutProcessor = wksprocess.NewDetailedJSONResultProcessor(c.rootConfig.Stdout)
+
+	}
+
+	wksProcessors := []wksprocess.Processor{
+		includeProcessor,
+		excludeProcessor,
+		wksprocess.NewHydrateLatestDetectionPlanProcessor(logger, repo),
+		wksprocess.NewFilterQueuedDriftDetectorProcessor(logger),
+		wksprocess.NewFilterDriftDetectionsBeforeProcessor(logger, c.notBefore),
+		wksprocess.NewSortByOldestDetectionPlanProcessor(logger),
+		wksprocess.NewLimitMaxProcessor(logger, c.maxPlans),
+		wksprocess.NewDriftDetectionPlanProcessor(logger, repo, c.planMessage),
+		wksprocess.NewDriftDetectionPlanWaitProcessor(logger, repo, waitPolling, c.waitTimeout),
+		resultOutProcessor,
+		wksprocess.NewDriftDetectionPlansResultProcessor(logger, c.disableDriftExitCode),
+	}
+
+	// Execute.
+	wks, err := repo.ListWorkspaces(ctx)
+	if err != nil {
+		return fmt.Errorf("could not list workspaces: %w", err)
+	}
+	chain := wksprocess.NewProcessorChain(wksProcessors)
+	_, err = chain.Process(ctx, wks)
+	if err != nil {
+		return fmt.Errorf("workspaces processing failed: %w", err)
+	}
+
+	return nil
+}
---
+++ cmd/tfe-drift/commands/version.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	"github.com/slok/tfe-drift/internal/info"
+)
+
+type VersionCommand struct {
+	cmd        *kingpin.CmdClause
+	rootConfig *RootCommand
+}
+
+// NewVersionCommand returns the version command.
+func NewVersionCommand(rootConfig *RootCommand, app *kingpin.Application) VersionCommand {
+	cmd := app.Command("version", "Shows version.")
+	c := VersionCommand{
+		cmd:        cmd,
+		rootConfig: rootConfig,
+	}
+
+	return c
+}
+
+func (v VersionCommand) Name() string { return v.cmd.FullCommand() }
+func (v VersionCommand) Run(ctx context.Context) error {
+	fmt.Fprintf(v.rootConfig.Stdout, info.Version)
+	return nil
+}
